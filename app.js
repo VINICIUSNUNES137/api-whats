845c439729b06fcbd2af9a7e025bda39f4ba2e62
VINICIUS NUNES DE OLIVEIRA <22282114@CFP127ALUNO.LOCAL>
2023-03-23 08:21:25 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const contatosImportado = require('./contatos.js')
+
+const app = express();
+
+app.use((request, response, next) => {
+    /*Permite gerenciar a origem das requisições da API,
+     * o (*) significa que será pública
+     * caso não for, deve ser escrito o IP
+     */
+    response.header("Access-Control-Allow-Origin", "*");
+  
+    /*Permite gerenciar quais métodos tem liberação para a requisição*/
+    response.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    /*Ativa no cors das requisições as permissões estabelecidas*/
+    app.use(cors());
+  
+    next();
+  })
+
+
+
+  app.get('/v1/senai/contato', cors(), async function(request, response, next){
+  
+    /**
+     *  Usei a query para receber diversas variaveis para realizar filtros,
+     * também usei o paramas para receber o ID (PK), 
+     * 
+     * 
+     */
+    // Recebe o valor da váriavel que é enviada por QueryString
+    let id = request.query.uf
+  
+      let statusCode
+    let dadosContato = {}
+  
+    if(id == '' || id == undefined || id.length != 1 || isNaN(id)){
+      statusCode = 400
+      dadosContato.message =  "Não é possível processar a requisição, pois o id do contato não foi informado ou não atende a quantidade de caracteres (1)."
+    }else{
+      let contatos = contatosImportado.getContato(id)
+        if(contatos){
+          statusCode = 200
+          dadosContato = contatos
+        }else{
+          statusCode = 404
+        }
+    }
+    response.status(statusCode)
+    response.json(dadosContato)
+  })
+
+  app.listen(8080, function () {
+    console.log("Servidor aguardando requisições");
+  });
+  
